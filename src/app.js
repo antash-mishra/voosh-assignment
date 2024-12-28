0dc1ea1c7d08343fa2af29aebd0bd4b6928ab0ae
Antash <mishraantash34@gmail.com>
2024-12-28 18:43:22 +0000

    get artist added

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use('/api/v1', require('./routes/auth'));
 app.use('/api/v1/users', require('./routes/users'));
 app.use('/api/v1/artists', require('./routes/artists'));
-// app.use('/api/v1/albums', require('./routes/albums'));
+app.use('/api/v1/albums', require('./routes/albums'));
 // app.use('/api/v1/tracks', require('./routes/tracks'));
 // app.use('/api/v1/favorites', require('./routes/favorites'));
 
