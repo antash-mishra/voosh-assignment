747e8537711635c2dfd14560c38c79fbee738028
Antash <mishraantash34@gmail.com>
2024-12-28 17:33:26 +0000

    added artist deletion route to start album route

--- src/routes/artists.js
+++ src/routes/artists.js
@@ -6,11 +6,13 @@ const getArtists = require('../controllers/getArtists');
 const getArtistById = require('../controllers/getArtistById');
 const addArtist = require('../controllers/addArtist');
 const updateArtist = require('../controllers/updateArtist');
+const deleteArtist = require('../controllers/deleteArtist');
 
 // Artist Routes
 router.get('/', authenticate, getArtists);
 router.get('/:id', authenticate,validateObjectId, getArtistById);
 router.post('/add-artist', authenticate, addArtist);
-router.put('/:id', authenticate, updateArtist)
+router.put('/:id', authenticate, updateArtist);
+router.delete('/:id', authenticate, deleteArtist);
 
 module.exports = router;
